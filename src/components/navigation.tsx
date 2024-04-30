b5a6fc021167381369b2ca85d4999655304cd5f5
Jason Yang <jason-yang@users.noreply.github.com>
2024-04-30 01:22:28 -0700

    feat: add new badge to cost-benefit-analysis calculator

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,3 +1,4 @@
+import { Badge } from "@cloudscape-design/components";
 import SideNavigation from "@cloudscape-design/components/side-navigation";
 import { usePathname, useRouter } from "next/navigation";
 
@@ -7,8 +8,8 @@ export default function Navigation() {
 
   return <SideNavigation activeHref={pathname} items={[
     {type: 'link', text: 'Home', href:"/"},
-    {type: 'expandable-link-group', text: 'Calculators', href: "/calculators", items: [
-      {type: 'link', text: 'Cost benefit analysis', href: '/calculators/cost-benefit-analysis'},
+    {type: 'expandable-link-group', text: 'Calculators', href: "/calculators", defaultExpanded: true, items: [
+      {type: 'link', text: 'Cost benefit analysis', href: '/calculators/cost-benefit-analysis', info: <Badge color="green">New</Badge>},
     ]},
     {type: 'expandable-link-group', text: 'Database', href: "/database", items: [
       {type: 'link', text: 'Classes', href: '/database/classes'},
